4ca28b847b3c464d602859f9d9288676d3768c34
baris <baris@BARIS_SOMEROGLU>
2024-04-27 17:21:23 +0300

    Add project files.

---
+++ AverageOfExam/Average.cpp
@@ -0,0 +1,37 @@
+﻿/*
+In this code, we will get average of 3 exams with using function and pointer.
+
+Developer = Barış Someroğlu
+Date 0 27.04.2024 - 05:10 pm
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Function prototype
+double PointerAverage(double*, double*, double*);
+
+int main()
+{
+	double Exam1, Exam2, Exam3;
+
+	cout << "Please enter 1. exam's point = ";
+	cin >> Exam1;
+
+	cout << "\nPlease enter 2. exam's point = ";
+	cin >> Exam2;
+
+	cout << "\nPlease enter 3. exam's point = ";
+	cin >> Exam3;
+
+	cout << "\nAverage = " << PointerAverage(&Exam1, &Exam2, &Exam3) << endl; // Call function
+
+	return 0;
+}
+
+// Function definition
+double PointerAverage(double* ptrExam1, double* ptrExam2, double* ptrExam3)
+{
+	return (*ptrExam1 + *ptrExam2 + *ptrExam3) / 3;
+}
